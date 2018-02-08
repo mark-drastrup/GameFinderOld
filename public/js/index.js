b009a2f2a9a1c596e3fc23aeb83eebbcfa46e197
mark_drastrup <mark.drastrup@gmail.com>
2018-02-08 19:26:45 +0100

    Deploying existing directory

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 5
+  });
+
+  $('.carousel-games-like').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5
+});
+});
