2c58c21045e2f1b24ff66b293bb0f4806b32e766
mark_drastrup <mark.drastrup@gmail.com>
2018-02-15 21:36:41 +0100

    Adding styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,5 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:30,
-    responsiveClass:true,
-    nav: true,
-    dots: true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:true
-        },
-        1000:{
-            items:5,
-            nav:true,
-            loop:true
-        }
-    }
-})
-
-    $('.owl-carousel-result').owlCarousel({
+    $('.owl-carousel').owlCarousel({
         loop:true,
         margin:30,
         responsiveClass:true,
